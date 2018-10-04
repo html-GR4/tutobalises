0c53a62600a3333edce6562fca78a82f00df7a87
Morgannec <morganne.culas@hotmail.fr>
2018-10-04 16:43:13 +0200

    modif

---
+++ li.php
@@ -0,0 +1,74 @@
+<?php include ("Headerr.html"); ?>
+    <nav>
+        <div>
+                  <ul class="menu">
+        <li>Squelette
+            <ul class="sousmenu">
+                <li><a href="body.html">body</a></li>
+                <li><a href="footer.html">footer</a></li>
+                <li><a href="meta.html">meta</a></li>
+            </ul>
+        </li>
+        <li>Insertion
+            <ul class="sousmenu">
+                <li><a href="link.html">link</a></li>
+                <li><a href="textarea.html">textarea</a></li>
+                <li><a href="img.html">img</a></li>
+                <li><a href="form.html">form</a></li>
+                <li><a href="div.html">div</a></li>
+            </ul>
+        </li>
+        <li>Présentation
+            <ul class="sousmenu">
+                <li><a href="h1h6.html">h1, h6</a></li>
+                <li><a href="li.html">li</a></li>
+                <li><a href="ul.html">ul</a></li>
+                <li><a href="br.html">br</a></li>
+                <li><a href="title.html">title</a></li>
+                <li><a href="p.html">p</a></li>
+                <li><a href="u.html">u</a></li>
+                <li><a href="b.html">b</a></li>
+            </ul>
+        </li>
+        <li>Contact
+            <ul class="sousmenu">
+                <li><a href="mailto:thecleaurelie.a@codeur.online">  Thècle-Aurélie</a></li>
+                <li><a href="mailto:rayan.g@codeur.online">Rayan</a></li>
+                <li><a href="mailto:morganne.c@codeur.online">Morganne</a></li>
+                  <li><a href="mailto:robin.n@codeur.online">Robin</a></li>
+            </ul>
+                </li>
+            </ul>
+        </li>
+        
+    </ul>
+
+        </div>
+
+
+
+
+
+
+	</nav>
+
+     
+	
+	<footer>
+        <p class="li">La balise "li"<br><br></p>
+        <p><img src="images/captureli.jpg" class="imgbody"></p>
+         <p class="txt">L'élément HTML < li > est utilisé pour représenter un élément dans une liste. Il doit être contenu dans un élément parent : une liste ordonnée (< ol >), une liste non ordonnée (< ul >) ou un menu (< menu >).<br> Dans les menus et les listes non ordonnées, les éléments de liste sont habituellement affichés en utilisant des puces.<br> Dans les listes ordonnées, ils sont habituellement affichés avec compteur croissant à gauche, tel qu'un nombre ou une lettre.</p>
+        
+
+
+      
+		
+
+
+	</footer>
+		
+
+	
+
+</body>
+</html>
